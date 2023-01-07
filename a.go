0a46d3cdee1bb8e294ab59f029bde1b4595903cf
Radi <evaradi18@gmail.com>
2023-01-07 17:07:38 +0100

    text

--- a.go
+++ a.go
@@ -1,9 +1,25 @@
 package main
 
 import "fmt"
-import "rsc.io/quote"
 
+// import "si"
+
+// import "rsc.io/quote"
+
+
+var name1="Mearnsk"
+
+func getName ()string{
+	name1 := "Mein nama ist Ra "
+	name1="89"
+	return name1
+}
 func main() {
-    // fmt.Println("Hello, World!")
-	fmt.Println(quote.Go())
+	var name2 string
+	fmt.Scan(&name2)
+    fmt.Printf("Hello, R%vAdi%T!\n",name1,name1)
+    fmt.Println("Hello, Radi!",name2)
+    fmt.Println("Hello, World! "+name1,getName(),name1)
+
+	// fmt.Println(quote.Go())
 }
