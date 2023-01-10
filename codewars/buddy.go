43e6806372714784a8685df0a2b6d736e5c7a375
Radi <evaradi18@gmail.com>
2023-01-10 03:35:39 +0100

    kataPy

--- codewars/buddy.go
+++ codewars/buddy.go
@@ -1,5 +1,4 @@
 package kata
-
 func SumDivisors(n int)int{
   s:=0
   for i := 1; i < n; i++ {
@@ -9,15 +8,12 @@ func SumDivisors(n int)int{
   }
   return s
 }
-// the return is `nil` if there is no buddy pair
 func Buddy(start, limit int) []int {
-  var n int
   for i := start; i <= limit; i++ {
-    n =i
+    n :=i
     var m int = SumDivisors(n)-1
     if SumDivisors(m)-1==n && m>n {
     return []int{n,m}}
   }
-  // your code
   return nil
 }
